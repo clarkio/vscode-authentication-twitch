00af3f8d4efc457a5d2fd198d0541a50b636eecc
clarkio <clarkio@users.noreply.github.com>
2023-05-19 16:32:41 -0400

    chore: follow proper semantic-release guides

--- publish.release.config.js
+++ publish.release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: [{ name: 'main', prerelease: true }],
+  branches: ['main'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
